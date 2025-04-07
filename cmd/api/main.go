c8b4be361a2fead8373cb23281a7d87f87e1664b
Fraska Buana <davinfraska20@gmail.com>
2025-04-07 08:47:36 +0700

    fix: cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -46,12 +46,7 @@ func main() {
 	// Gin Initial;
 	router := gin.Default()
 
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Content-Type", "access-control-allow-origin", "access-control-allow-headers"},
-		AllowCredentials: true,
-	}))
+	router.Use(cors.Default())
 
 	initRoute(router, db, sdb, cache)
 
